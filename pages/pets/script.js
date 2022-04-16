7a987c5eea1d73486b43770e42937d25ecebf7b0
EnotPotaskyn <ackii@bk.ru>
2022-04-16 22:50:12 +0300

    feat:  lalalal

---
+++ pages/pets/script.js
@@ -0,0 +1,20 @@
+const iconMenu = document.querySelector('.header__pets__row__menu');
+const headNav = document.querySelector('.header__pets__row__nav');
+const navList = document.querySelector('.header__pets__row__nav__list');
+
+if(iconMenu){
+  iconMenu.addEventListener('click',function(){
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    headNav.classList.toggle('_active');
+  });
+}   
+
+if(navList){
+  navList.addEventListener('click',function(){
+    document.body.classList.remove('_lock');
+    iconMenu.classList.remove('_active');
+    headNav.classList.remove('_active');
+});
+}
+
